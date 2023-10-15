5c2b2107185954bd3dc19bdd3acca136c7075b88
Oliver Holmes <oliver.albert.holmes@gmail.com>
2023-10-15 12:08:01 +1030

    Add path to cdk stack

--- cdk/cdk/pipeline_stack.py
+++ cdk/cdk/pipeline_stack.py
@@ -24,53 +24,7 @@ class CodePipelineStack(Stack):
                 commands=[
                     "npm install -g aws-cdk",  # Installs the cdk cli on Codebuild
                     "pip install -r requirements.txt",  # Instructs Codebuild to install required packages
-                    "npx cdk synth",
+                    "npx cdk synth --app 'cdk'",
                 ]
             ),
         )
-
-
-# class CdkStack(Stack):
-
-#     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
-#         super().__init__(scope, construct_id, **kwargs)
-
-#         queue = sqs.Queue(
-#             self, "CdkQueue",
-#             visibility_timeout=Duration.seconds(300),
-#         )
-
-#         topic = sns.Topic(
-#             self, "CdkTopic"
-#         )
-
-#         topic.add_subscription(subs.SqsSubscription(queue))
-
-
-
-# from constructs import Construct
-# from aws_cdk import (
-#     Duration,
-#     Stack,
-#     aws_iam as iam,
-#     aws_sqs as sqs,
-#     aws_sns as sns,
-#     aws_sns_subscriptions as subs,
-# )
-
-
-# class CdkStack(Stack):
-
-#     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
-#         super().__init__(scope, construct_id, **kwargs)
-
-#         queue = sqs.Queue(
-#             self, "CdkQueue",
-#             visibility_timeout=Duration.seconds(300),
-#         )
-
-#         topic = sns.Topic(
-#             self, "CdkTopic"
-#         )
-
-#         topic.add_subscription(subs.SqsSubscription(queue))
